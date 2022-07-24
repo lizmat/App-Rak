52aae0f99614937a5c416d8e5beb4d0ad98b5813
Alexey Melezhik <melezhik@gmail.com>
2022-07-24 18:12:37 -0500

    debugging

--- .tomty/tasks/rak-search/task.bash
+++ .tomty/tasks/rak-search/task.bash
@@ -1,9 +1,12 @@
 set -e
+cat $root_dir/task.bash
 
 echo "foo" > $cache_root_dir/file.txt
 cd $cache_root_dir
 
-echo "run: rak foo --highlight=False --human"
+echo "====="
+cat file.txt
+echo "====="
 
 echo "<<<"
 rak foo --highlight=False --human 2>&1
