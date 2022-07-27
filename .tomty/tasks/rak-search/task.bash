d829d11e21a0f119c1519c4dd8f232ce2cfb71c9
Alexey Melezhik <melezhik@gmail.com>
2022-07-26 19:00:08 -0500

    continue debugging

--- .tomty/tasks/rak-search/task.bash
+++ .tomty/tasks/rak-search/task.bash
@@ -3,15 +3,19 @@ set -e
 cat $root_dir/task.bash
 
 echo "foo" > $cache_root_dir/file.txt
+echo "foo2" >> $cache_root_dir/file.txt
+echo "foo3" >> $cache_root_dir/file.txt
 
 cd $cache_root_dir
+
+which rak
+rak --version
+ls -l
+pwd
 
 echo "====="
 cat file.txt
 echo "====="
-
-which rak
-rak --version
 
 echo "<<<"
 #rak foo --/highlight --human
